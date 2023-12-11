45d5b38bea8b295d8c0ce023f9122b2d2d291144
ituitis-altintasb18 <altintasb18@itu.edu.tr>
2023-12-11 12:13:19 +0300

    'Update0'

---
+++ src/components/UserPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const UserPage = () => {
+  // Replace this with your user data
+  const user = {
+    username: 'example_user',
+    followers: 1000,
+    achievementsLink: '/achievements',
+    cardsLink: '/cards',
+    // Assuming your images are stored in the public folder
+    profilePicture: '/images/profile.jpg',
+  };
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <img src={user.profilePicture} alt="Profile" style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
+      <h2>{user.username}</h2>
+      <p>Followers: {user.followers}</p>
+      <p>
+        <a href={user.achievementsLink}>Achievements Page</a>
+      </p>
+      <p>
+        <a href={user.cardsLink}>Showcase Cards</a>
+      </p>
+    </div>
+  );
+};
+
+export default UserPage;
